1d92b6d094310f125668c32792774aa59de397f2
OlegVladimirovichG <hankat@mail.ru>
2022-11-22 20:52:54 +0300

    controller.py

---
+++ controller.py
@@ -0,0 +1,12 @@
+import view
+import model
+
+def run():
+    num = 1
+    while num > 0:
+        view.print_menu()
+        num = int(input('Выберети действие: '))
+        if num == 1:
+            pass
+        elif num == 2:
+            pass
